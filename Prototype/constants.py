bafc570e76bd69fc96ea404e993bdf8a93ca2c9e
seungyoon95 <59849573+seungyoon95@users.noreply.github.com>
2025-01-15 05:51:39 -0800

    Flood detection optimization to reduce false positives

--- Prototype/constants.py
+++ Prototype/constants.py
@@ -1,2 +1,4 @@
 THRESHOLD = 100     # number of packets from single source before identifying as DOS attack
 TIMEFRAME = 1       # seconds
+
+MAX_PENDING = 50
--- Prototype/detector.py
+++ Prototype/detector.py
@@ -5,16 +5,20 @@ from datetime import datetime
 import time
 import os
 import socket
-from collections import defaultdict
+from collections import defaultdict, deque
 from scapy.layers.inet import IP
-
-local_ip = socket.gethostbyname(socket.gethostname())
 
 
 # Dictionaries to hold packet counts from 
 syn_count = defaultdict(list)
 udp_count = defaultdict(list)
 icmp_count = defaultdict(list)
+
+pending_handshake = defaultdict(deque)
+
+
+# Host IP address
+local_ip = socket.gethostbyname(socket.gethostname())
 
 
 # Writes packet info to a log file
@@ -38,25 +42,51 @@ def write_to_log(packet):
 def syn_flood(packet):
     if packet.haslayer('TCP') and packet.haslayer('IP'):
         source_ip = packet['IP'].src
+        dst_ip = packet['IP'].dst
+
         current_time = time.time()
 
-        syn_count[source_ip].append(current_time)
-        syn_count[source_ip] = [t for t in syn_count[source_ip] if current_time - t < constants.TIMEFRAME]
+        if packet['TCP'].flags == 'S':
+            syn_count[source_ip].append(current_time)
+            syn_count[source_ip] = [t for t in syn_count[source_ip] if current_time - t < constants.TIMEFRAME]
 
-        if len(syn_count[source_ip]) > constants.THRESHOLD:
-            if source_ip != local_ip:
-                print(f"***ALERT*** SYN Flood Attack detected from: {source_ip}")
-                write_to_log(packet)
+            pending_handshake[(source_ip, dst_ip)].append(current_time)
+
+            # Remove old SYNs from the queue
+            while pending_handshake[(source_ip, dst_ip)] and current_time - pending_handshake[(source_ip, dst_ip)][0] > constants.TIMEFRAME:
+                pending_handshake[(source_ip, dst_ip)].popleft()
+
+            # Check for SYN flood
+            if len(syn_count[source_ip]) > constants.THRESHOLD or len(pending_handshake[(source_ip, dst_ip)]) > constants.MAX_PENDING:
+                if source_ip != local_ip:
+                    print(f"***ALERT*** SYN Flood Attack detected from: {source_ip}")
+                    write_to_log(packet)
+
+            # Check if this is an ACK packet (ACK flag set, SYN flag not set)
+        elif packet['TCP'].flags == 'A':
+            if (source_ip, dst_ip) in pending_handshake:
+                if pending_handshake[(source_ip, dst_ip)]:
+                    # Remove the oldest pending SYN since it has been acknowledged
+                    pending_handshake[(source_ip, dst_ip)].popleft()    
+
+        # if len(syn_count[source_ip]) > constants.THRESHOLD:
+        #     if source_ip != local_ip:
+        #         print(f"***ALERT*** SYN Flood Attack detected from: {source_ip}")
+        #         write_to_log(packet)
                 
 
 # Detects UDP Flood Attack based on given timeframe and threshold
 def udp_flood(packet):
     if packet.haslayer('UDP') and packet.haslayer('IP'):
+        whitelisted_port = [53, 123]
+
         source_ip = packet['IP'].src
+        dst_port = packet['IP'].dport
         current_time = time.time()
 
-        udp_count[source_ip].append(current_time)
-        udp_count[source_ip] = [t for t in udp_count[source_ip] if current_time - t < constants.TIMEFRAME]
+        if (dst_port not in whitelisted_port):
+            udp_count[source_ip].append(current_time)
+            udp_count[source_ip] = [t for t in udp_count[source_ip] if current_time - t < constants.TIMEFRAME]
 
         if len(udp_count[source_ip]) > constants.THRESHOLD:
             if source_ip != local_ip:
