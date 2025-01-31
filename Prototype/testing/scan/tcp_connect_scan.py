eacfcb2649d1837905549f367b6e9d23466c2e0d
seungyoon95 <59849573+seungyoon95@users.noreply.github.com>
2025-01-30 22:06:18 -0800

    scan tests created

--- Prototype/testing/scan/tcp_connect_scan.py
+++ Prototype/testing/scan/tcp_connect_scan.py
@@ -1,1 +1,26 @@
+from scapy.all import send
+from scapy.layers.inet import IP, TCP, sr1
 
+def tcp_connect_scan(target_ip, start, end):
+    for target_port in range(start, end + 1):
+        ip = IP(dst=target_ip)
+        syn = TCP(dport=target_port, flags="S")
+        response = sr1(ip/syn, timeout=1, verbose=0)
+
+        if response:
+            if response.haslayer(TCP):
+                if response.getlayer(TCP).flags == 0x12:
+                    print(f"Port {target_port} is OPEN (TCP Connect Scan)")
+                    ack = TCP(dport=target_port, flags="A", seq=response.seq + 1, ack=response.ack)
+                    send(ip/ack, verbose=0)
+                elif response.getlayer(TCP).flags == 0x14:
+                    print(f"Port {target_port} is CLOSED (TCP Connect Scan)")
+        else:
+            print(f"No response from {target_ip} on port {target_port}")
+
+
+target_ip = "192.168.56.1"
+start = 10000
+end = 10011
+
+tcp_connect_scan(target_ip, start, end)
--- Prototype/testing/scan/xmas_scan.py
+++ Prototype/testing/scan/xmas_scan.py
@@ -1,1 +1,23 @@
+from scapy.layers.inet import IP, TCP, sr1
 
+def xmas_scan(target_ip, target_port):
+    ip = IP(dst=target_ip)
+    xmas = TCP(dport=target_port, flags="FPU")
+    response = sr1(ip/xmas, timeout=1, verbose=0)
+
+    if response:
+        if response.haslayer(TCP):
+            if response.getlayer(TCP).flags == 0x14:
+                print(f"Port {target_port} is CLOSED (Xmas scan)")
+            elif response.getlayer(TCP).flags == 0x12:
+                print(f"Port {target_port} is OPEN (Xmas scan)")
+        else:
+            print(f"Received unexpected response from {target_ip}")
+    else:
+        print(f"No response from {target_ip} on port {target_port}")
+
+
+target_ip = "192.168.56.1"
+target_port = 10004
+
+xmas_scan(target_ip, target_port)
