016fef0914acc724fe45f4a2f68dfac93b9858f5
seungyoon95 <59849573+seungyoon95@users.noreply.github.com>
2025-03-06 01:16:27 -0800

    .

--- Prototype/constants.py
+++ Prototype/constants.py
@@ -1,8 +1,8 @@
-FLOOD_THRESHOLD = 500     # number of packets from single source before identifying as DOS attack
-SCAN_THRESHOLD = 5
+FLOOD_THRESHOLD = 500   # number of packets from single source before identifying as DOS attack
+SCAN_THRESHOLD = 5      # number of ports before identifying as scan attempt
 
 MAX_PENDING = 50
 
-SSH_THRESHOLD = 5
+SSH_THRESHOLD = 5       # number of brute force attempts
 
-TIMEFRAME = 1       # seconds
+TIMEFRAME = 1           # seconds
